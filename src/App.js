c4d89523ee5b49b82a56003096f71f6731839fac
Felipe Silva <felipecacezi@gmail.com>
2023-11-01 18:35:50 -0300

    criação da lista de todos e estilização da lista

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const ESCAPE_KEY = 27;
+    const ENTER_KEY = 13;
+
+    const initialTodos = [
+        { id: 1, title: 'Estudar react', checked: false },
+        { id: 2, title: 'Estudar inglês', checked: true },
+        { id: 3, title: 'Estudar frances', checked: false }
+    ];
+    const [todos, setTodos] = useState(initialTodos);
+    const [ value, setValue ]= useState('');
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const submit = () => {
+        console.log('submit', value);
+        erase();
+    }
+
+    const erase = () => {
+        setValue('');
+    }
+
+    const onKeyDown = (event) => {
+
+        switch (event.which) {
+            case ESCAPE_KEY:
+                erase();
+            break;
+            case ENTER_KEY:
+                submit();
+            break;
+            default:
+                return;
+            break;
+        }
+
+    }
+
+    return (
+    
+        <section id='app' className='container'>
+    
+            <header>
+
+                <h1 className='title'>Todo App</h1>
+
+            </header>
+    
+            <section className='main'>
+
+                <input 
+                    className='new-todo'
+                    placeholder='O que precisa ser feito?'
+                    value={value}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}/>
+
+                <ul className='todo-list'>
+                    {
+                        todos.map((todo)=>(
+                            <li key={todo.id}>
+                                <span className='todo'>{todo.title}</span>
+                                <button className='remove'
+                                        type="button">
+                                    <MdDelete size={28}/>
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+            </section>
+    
+        </section>
+    
+    );
 }
 
 export default App;
